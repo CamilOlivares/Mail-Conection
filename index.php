13c79ff4ed67f6f4ccc45dfb50cae5645e1c5ff3
Camilo Olivares <140129147+CamilOlivares@users.noreply.github.com>
2025-02-22 15:09:55 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html> <!-- Define el tipo de documento como HTML5 -->
+<html lang="es"> <!-- Indica que el idioma del contenido es español -->
+<head>
+    <meta charset="UTF-8"> <!-- Define la codificación de caracteres como UTF-8 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Configura el viewport para un diseño responsivo -->
+    <title>Mi Portafolio</title> <!-- Título de la página que aparece en la pestaña del navegador -->
+    <!-- Enlaces a hojas de estilo CSS -->
+
+    <link rel="stylesheet" href="assets/css/contact.css"> <!-- aqui colocas tu estilo de CSS que prefieras (no viene incluido,revisar en seccion de estilos de formulario) -->
+
+</head>
+<body>
+    
+
+
+<!-- Incluye el contenido del formulairo de contacto -->
+ <?php include("views/contact.php"); ?> 
+
+
+<!-- Scripts para el cargador de la página -->
+<script src="js/recaptcha.js"></script>
+
+
+
+</body>
+</html>
