4f442ef3cf491eeed207b62b6bee71fce7833192
Camilo Olivares <140129147+CamilOlivares@users.noreply.github.com>
2025-02-22 15:10:52 -0300

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,25 @@
+<section id="contact" data-aos="fade-up">
+    <div class="container">
+        <h2>Contacto</h2>
+        <form id="contact-form" action="/back/envio-formulario.php" method="post">
+            <label for="name">Nombre:</label>
+            <input type="text" id="name" name="name" required>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            <label for="message">Mensaje:</label>
+            <textarea id="message" name="message" required></textarea>
+
+            <button class="g-recaptcha"
+                    data-sitekey="6Ld_u78qAAAAAJ66_Io5STZwNarXCtXyv66bUhse"
+                    data-callback="onSubmit"
+                    data-action="submit">Enviar</button>
+        </form>
+    </div>
+</section>
+
+<script src="https://www.google.com/recaptcha/api.js"></script>
+<script>
+    function onSubmit(token) {
+        document.getElementById("contact-form").submit();
+    }
+</script>
